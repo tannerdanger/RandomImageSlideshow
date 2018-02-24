c1ff2527f0f08c6aabc70f67d036ca8d88d32c80
Tanner Brown <tannersbrown@gmail.com>
2018-02-24 12:24:31 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //create a file chooser, pass in file
+        new SlideShow();
+
+
+
+    }
+}
